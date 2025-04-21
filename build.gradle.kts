4eba248df5ca343b17a9c7a30dd34b6642bb76ee
Eugen Martynov <eugenemartynov@gmail.com>
2025-04-21 14:26:41 +0200

    Add version catalogue update plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,13 @@
+import nl.littlerobots.vcu.plugin.resolver.VersionSelectors.Companion.PREFER_STABLE
+
 plugins {
     alias(libs.plugins.androidLibrary) apply false
-    alias(libs.plugins.kotlinMultiplatform) apply  false
+    alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.vanniktech.mavenPublish) apply false
+    alias(libs.plugins.versions.catalogue.update)
+}
+
+versionCatalogUpdate {
+    sortByKey = true
+    versionSelector(PREFER_STABLE)
 }
