d4651de39d4b0dfd8e4da8806ee83b2e68055482
Eugen Martynov <eugenemartynov@gmail.com>
2025-04-21 21:48:34 +0200

    Rework plugins notation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 import nl.littlerobots.vcu.plugin.resolver.VersionSelectors.Companion.PREFER_STABLE
 
 plugins {
-    alias(libs.plugins.androidLibrary) apply false
-    alias(libs.plugins.kotlinMultiplatform) apply false
-    alias(libs.plugins.vanniktech.mavenPublish) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.multiplatform) apply false
+    alias(libs.plugins.maven.publish) apply false
     alias(libs.plugins.versions.catalogue.update)
 }
 
