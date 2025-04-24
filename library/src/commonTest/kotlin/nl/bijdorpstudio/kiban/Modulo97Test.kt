fe200e5b559f5b048a1bcddae98bdca10a462daa
Eugen Martynov <eugenemartynov@gmail.com>
2025-04-24 22:15:54 +0200

    Update copyright

--- library/src/commonTest/kotlin/nl/bijdorpstudio/kiban/Modulo97Test.kt
+++ library/src/commonTest/kotlin/nl/bijdorpstudio/kiban/Modulo97Test.kt
@@ -1,5 +1,5 @@
 /*
-   Original copyright 2021 Barend Garvelink
+   Copyright 2021 Barend Garvelink, Eugen Martynov
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
