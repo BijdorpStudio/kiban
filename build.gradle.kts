74a52d699e233a1190bbe7fecb5662d1090aa265
Eugen Martynov <eugen.martynov@nl.abnamro.com>
2025-05-20 21:45:25 +0200

    Fix gradle setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import nl.littlerobots.vcu.plugin.resolver.VersionSelectors.Companion.PREFER_STA
 plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.multiplatform) apply false
+    alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.maven.publish) apply false
     alias(libs.plugins.versions.catalogue.update)
     alias(libs.plugins.compat.patrouille) apply false
--- java-iban-compat/build.gradle.kts
+++ java-iban-compat/build.gradle.kts
@@ -1,27 +1,17 @@
-plugins {
-    kotlin("jvm") version "1.9.22" // Or your project's Kotlin version
-}
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
-repositories {
-    mavenCentral()
+plugins {
+    alias(libs.plugins.kotlin.jvm)
 }
 
 dependencies {
     implementation(project(":library"))
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    testImplementation("junit:junit:4.13.2")
 }
 
 kotlin {
-    jvmToolchain(11)
-}
+    jvmToolchain(17)
 
-// Specify source sets if not automatically inferred, though Gradle usually infers this correctly
-// sourceSets {
-//     main {
-//         kotlin.srcDir("src/main/kotlin")
-//     }
-//     test {
-//         kotlin.srcDir("src/test/kotlin")
-//     }
-// }
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_17)
+    }
+}
