abbe29fcebdbd5130998faf2525e075e83d69d00
Eugen Martynov <eugen.martynov@nl.abnamro.com>
2025-05-24 22:57:25 +0200

    Return back kmp plugin

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,6 +1,7 @@
 import com.vanniktech.maven.publish.SonatypeHost
 
 plugins {
+    alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.android.library)
     alias(libs.plugins.compat.patrouille)
     alias(libs.plugins.maven.publish)
